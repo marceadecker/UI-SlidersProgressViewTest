ef1c209bdd444e9459caa94cb37df6373c155ca8
marceadecker <hello@marcea.de>
2015-06-17 14:08:39 -0400

    UISliders Progress View
    Test of functionality with timer & buttons.

--- UI-SlidersProgressView/ViewController.swift
+++ UI-SlidersProgressView/ViewController.swift
@@ -9,10 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var myTimer = NSTimer()
 
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBOutlet weak var myProgView: UIProgressView!
+    
+    @IBAction func myButtonPressed(sender: UIButton) {
+        myTimer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("myFunction"), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func stopButton(sender: UIButton) {
+        
+        myTimer.invalidate()
+    }
+    
+    func myFunction(){
+        myProgView.progress = myProgView.progress + 0.1
+        myLabel.text = "progress: \(myProgView.progress)"
+    }
+    
+    @IBAction func sliderMoved(sender: UISlider) {
+        println("slider is at value \(sender.value)")
+        
+        myProgView.progress = sender.value
+        myLabel.text = "progress: \(myProgView.progress)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myProgView.progress = 0.0
+        myLabel.text = "progress: \(myProgView.progress)"
     }
 
     override func didReceiveMemoryWarning() {
